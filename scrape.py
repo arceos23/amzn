1ba21ac48b33b6fb78be487403cedc94bdaa7a30
Shane Arceo <arceoshane23@gmail.com>
2025-01-07 13:44:42 -0500

    created a web scraper to locate files to download from the Amazon Reviews URL data source based on product category

---
+++ scrape.py
@@ -0,0 +1,59 @@
+import os
+import time
+import requests
+from bs4 import BeautifulSoup
+
+AMAZON_REVIEWS_URL = "https://amazon-reviews-2023.github.io/"
+TIMEOUT_SECONDS = "5"
+TABLE_ID = "grouped-by-category"
+ANCHOR_TAG = "a"
+DOWNLOAD_ATTRIBUTE = "download"
+HREF = "href"
+FOLDER_NAME = "downloads"
+TARGET_PRODUCT_CATEGORY = "Subscription_Boxes"
+
+
+def main():
+    try:
+        # Get HTML from URL
+        r = requests.get(AMAZON_REVIEWS_URL, TIMEOUT_SECONDS)
+        r.raise_for_status()  # Raises an error for a 4XX or 5XX status code
+
+        # Retrieve the reviews and metadata download links for each product category
+        soup = BeautifulSoup(r.content, "html.parser")
+        table = soup.find(id=TABLE_ID)
+        links = table.find_all(ANCHOR_TAG)
+        download_links = [
+            link
+            for link in links
+            if link.get(DOWNLOAD_ATTRIBUTE) == "" and link.get(HREF) is not None
+        ]
+
+        # Save review and metadata files for the target download category
+        for link in download_links:
+            if TARGET_PRODUCT_CATEGORY in link[HREF]:
+                url = link[HREF]
+                tokens = url.strip().split("/")
+
+                # Make folder to store downloaded file
+                category = tokens[-2]
+                folder_path = os.path.join(os.getcwd(), f"{FOLDER_NAME}/{category}")
+                os.makedirs(folder_path, exist_ok=True)
+
+                # Store file
+                filename = tokens[-1]
+                file_path = os.path.join(folder_path, filename)
+                start_time = time.perf_counter()
+                download_response = requests.get(url, TIMEOUT_SECONDS)
+                with open(file_path, "wb") as file:
+                    file.write(download_response.content)
+                end_time = time.perf_counter()
+                print(
+                    f"Successfully saved file from {url} to {file_path} in {end_time - start_time} seconds.\n"
+                )
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+
+
+if __name__ == "__main__":
+    main()
