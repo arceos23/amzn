a16387741f0d2c515549e33708ce820fea43e2e5
Shane Arceo <arceoshane23@gmail.com>
2025-03-09 12:54:38 -0400

    implemented basic search by word in title using elasticsearch

--- api/api.py
+++ api/api.py
@@ -1,25 +1,43 @@
 from fastapi import FastAPI
 import psycopg
+from elasticsearch_dsl import connections
+import os
+from elasticsearch_dsl import Search
 
 app = FastAPI()
 
 
 @app.get("/products")
-async def root():
-    # Connect to an existing database
-    with psycopg.connect(
-        "host=localhost port=5430 connect_timeout=10 user=postgres password=1234"
-    ) as conn:
-        with conn.cursor() as cur:
-            return {
-                record
-                for record in cur.execute(
+async def root(title=""):
+    try:
+        if not title:
+            with psycopg.connect(
+                "host=localhost port=5430 connect_timeout=10 user=postgres password=1234"
+            ) as conn:
+                with conn.cursor() as cur:
+                    return {
+                        record
+                        for record in cur.execute(
+                            """
+                        SELECT id, main_category, title, average_rating, rating_number, features, description, price, store, parent_asin
+                        FROM products;
                     """
-                SELECT id, main_category, title, average_rating, rating_number, features, description, price, store, parent_asin
-                FROM products;
-            """
-                )
-            }
+                        )
+                    }
+
+        connections.create_connection(
+            hosts="http://localhost:9200",
+            basic_auth=(
+                os.environ.get("ES_LOCAL_USER"),
+                os.environ.get("ES_LOCAL_PASSWORD"),
+            ),
+        )
+        s = Search(index="product").query("match", title=title)
+        response = s.execute()
+        return [hit for hit in response]
+
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
 
 
 @app.get("/reviews")
